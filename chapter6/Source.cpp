dff034143798b97935ca8cc4ee11d7eec824ef6c
AbsoluteAbsolver <64785006+AbsoluteAbsolver@users.noreply.github.com>
2020-10-22 23:27:28 +0200

    addes hello in comments

--- chapter6/Source.cpp
+++ chapter6/Source.cpp
@@ -2,7 +2,7 @@
 //
 // This is example code from Chapter 6.7 "Trying the second version" of
 // "Software - Principles and Practice using C++" by Bjarne Stroustrup
-//
+// hello
 
 /*
     This file is known as calculator02buggy.cpp
